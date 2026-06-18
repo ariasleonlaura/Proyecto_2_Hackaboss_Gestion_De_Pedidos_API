ee2f48c3d8aed1b646d52b1e60df793304189ee2
RafaPorcel <rafaelporcelllobregat@gmail.com>
2026-06-18 20:13:01 +0200

    feat: base de AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,3 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Importamos el Layout (el marco común, como un menú superior)
+import Layout from '../components/common/Layout';
+
+// Importamos las páginas
+import TerminalPage from '../pages/TerminalPage';
+import CocinaPage from '../pages/CocinaPage';
+import RecogidaPage from '../pages/RecogidaPage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 export default AppRouter;
